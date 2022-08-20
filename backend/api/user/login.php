72c1d64e2d62e890b3864737827f1f6670ce0892
derick <derick@localhost>
2022-08-20 11:58:42 +0300

    modified login

--- backend/api/user/login.php
+++ backend/api/user/login.php
@@ -14,10 +14,13 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-//function to send sms to
+//function to send sms to using adb shell "am start-activity -n com.android.shellsms/.MainActivity --es pass sendsms --es dst +254702480601 --es sms 'Hello'
 function send_sms($destination,$token){
-$sms="Your CUEA code:\n".$token."\n";
-//echo $sms;
+	$sms="Your CUEA Code:\n".$token."\n Do not Share this Code";
+	$cmd="adb shell \"am start-activity -n com.android.shellsms/.MainActivity --es dst $destination --es sms '$sms'\" ";
+	//execute
+	shell_exec($cmd);
+	sleep(1);
 }
 
 //function to hide the number before sending it to user
@@ -46,13 +49,13 @@ if(isset($data->AdmissionNo)){
     //Check if the admission exists in the database
     $exists=$user->setUserProfile($data->AdmissionNo);
     if($exists){
+	 $fname=$user->getFirstName();
+	 $phone=$user->getPhoneNo();
         //generate a token for the user 
         $token=rand(100,999).'-'.rand(100,999);
         //insert token to db
         if($user->setToken($token)){
-            send_sms(1,$token);
-	    $fname=$user->getFirstName();
-	    $phone=$user->getPhoneNo();
+            send_sms($phone,$token);
             http_response_code(200);
             echo json_encode(array("message"=>"proceed to verification","firstname"=>$fname,"phoneno"=>hide_number($phone)));
         }
